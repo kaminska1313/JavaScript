e6183558e6ac9017b02d718970a8251f88eb4843
Tatiana Kaminska <105613820+kaminska1313@users.noreply.github.com>
2022-06-16 00:40:49 +0300

    Update kot_essa_JS_homework.js

--- kot_essa_JS_homework.js
+++ kot_essa_JS_homework.js
@@ -29,7 +29,7 @@ console.log(item_4);
 // 14. Вывести в консоль умножение item_3 и item_4.
 
 console.log(item_3 + item_4);
-console.log(item_3 * item_4);
+console.log(typeof item_3 * item_4);
 
 // 15. Создать переменную “item_5”
 // 16. Присвоить переменной item_5 переменную item_3
@@ -48,8 +48,7 @@ let item_6_type;
 item_6 = 15;
 item_6_type = typeof (item_6);
 
-console.log('item_6 ==', item_6, "type is", typeof item_6);
-console.log('item_6_type ==', item_6_type, "type is", typeof item_6_type);
+console.log("item_6 ==", item_6, "item_6_type ==", item_6_type);
 
 // 22. Создать переменную item_7 и в ней преобразовать item_6 в String.
 
@@ -61,9 +60,7 @@ let item_7 = item_6.toString();
 // 25. Вывести в консоль тип данных item_7 в виде ——  “item_7 == ”  item_7,  “item_7_type == ”  item_7_type ——  
 
 let item_7_type = typeof item_7;
-
-console.log('item_7 ==', item_7, "type is", typeof item_7);
-console.log('item_7_type ==', item_7_type, "type is", typeof item_7_type);
+console.log("item_7 ==", item_7, "item_7_type ==", item_7_type);
 
 // 26. Создать переменную “age_1” и присвоить ей значение 10
 // 27. Создать переменную “age_2” и присвоить ей значение 18
@@ -76,7 +73,7 @@ let age_3 = 60;
 //  29. Создать if в котором будите проверять значение переменной age_1
 //  30. Если age_1 < age_2, вывести в консоль “You don’t have access cause your age is ” + age_1 + “ It’s less then ”
 //  31. Если age_1 >=  age_2 и age_1 <  age_3, вывести в консоль “Welcome  !”
-//  32. Если age_1  >= age_3, вывести в консоль “Keep calm and look Culture channel”.
+//  32. Если age_1  > age_3, вывести в консоль “Keep calm and look Culture channel”.
 //  33. Иначе выводите “Technical work”.
 
 
@@ -84,7 +81,7 @@ if (age_1 < age_2) {
     console.log('You don’t have access cause your age is ' + age_1 + ' It’s less then ' + age_2);
 } else if (age_1 >= age_2 && age_1 < age_3) {
     console.log('Welcome!');
-} else if (age_1 > age_3) {
+} else if (age_1 >= age_3) {
     console.log('Keep calm and look Culture channel');
 } else {
     console.log('Technical work');
