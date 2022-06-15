7eaacf45f9803ead5b0b9e1066bff625660a601d
Tatiana Kaminska <105613820+kaminska1313@users.noreply.github.com>
2022-06-16 00:44:14 +0300

    Update kot_essa_JS_checkAge4.js

--- kot_essa_JS_checkAge4.js
+++ kot_essa_JS_checkAge4.js
@@ -2,25 +2,20 @@
 // Преобразовать задание 3* таким образом, чтобы возраст вводится используя функцию prompt в привязанной верстке
 //ссылаюсь на этот скрипт в файле checkAge.html
 
-
 const checkAge = function (age) {
-
-    let age_2 = 18;
-    let age_3 = 60;
 
     if (isNaN(age) || !age) {
         alert('Error!!!');
-    } else if (age < age_2) {
-        alert(`You don’t have access cause your age is ${age} It’s less then ${age_2}`);
-    } else if (age >= age_2 && age < age_3) {
+    } else if (age < 18) {
+        alert(`You don’t have access cause your age is ${age} It’s less then 18`);
+    } else if (age >= 18 && age < 60) {
         alert('Welcome!');
-    } else if (age >= age_3) {
-        alert('Keep calm and look Culture channel');
+    } else if (age >= 60) {
+        alert('Keep calm and watch Culture channel');
     } else {
         alert('Technical work');
     }
 }
-
 let question = prompt('How old are you?');
 checkAge(question)
 
