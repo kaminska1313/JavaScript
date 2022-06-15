e7a33a674f745e2d5426e88d9a3c38e096a0f9d3
Tatiana Kaminska <105613820+kaminska1313@users.noreply.github.com>
2022-06-15 23:30:43 +0300

    Update kot_essa_JS_checkAge2.js

--- kot_essa_JS_checkAge2.js
+++ kot_essa_JS_checkAge2.js
@@ -13,7 +13,7 @@ const checkAge = function (age) {
         console.log(`You don’t have access cause your age is ${age} It’s less then ${age_2}`);
     } else if (age >= age_2 && age < age_3) {
         console.log('Welcome!');
-    } else if (age > age_3) {
+    } else if (age >= age_3) {
         console.log('Keep calm and look Culture channel');
     } else {
         console.log('Technical work');
